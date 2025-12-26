fb624543cf515edd101569e451c3de8c4ee58f7c
odiljon888 <xamidjonovodiljon92@gmail.com>
2025-12-26 16:25:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let namee = prompt("Familiyangizni kiriting");
+let name = prompt("Ismingizni kiriting");
+let age = prompt("Yoshingizni kiriting");
+let nama = prompt("Yoqtirgan kasbingiz");
+let nmaa = prompt("Yoqtirgan oyiningiz nomi")
+
+
+console.log("Familiyangiz " + namee + " Ismingiz " + name + " Yoshingiz " + age + " Kasbingiz " + nama + "Oyiningiz" + nmaa)
+  
+
